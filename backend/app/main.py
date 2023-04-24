e60e2eb89456cace69c377656b8b47921089e3a7
Grégoire Van Thienen <gregoire.vanthienen@outlook.com>
2023-04-25 01:51:02 +0200

    added cors

--- backend/app/main.py
+++ backend/app/main.py
@@ -11,9 +11,23 @@ from app.core.auth import get_current_active_user
 #from app.core.celery_app import celery_app
 #from app import tasks
 
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI(
     title=config.PROJECT_NAME, docs_url="/api/docs", openapi_url="/api"
+)
+
+origins = [
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 
